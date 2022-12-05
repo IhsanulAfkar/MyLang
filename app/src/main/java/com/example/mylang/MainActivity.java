15095cd76acd833a56c5132d72c25608cfc061c4
FarisAnwari <mfarisanwari@gmail.com>
2022-12-05 17:20:22 +0700

    splash distorted, looking for fix

--- app/src/main/java/com/example/mylang/MainActivity.java
+++ app/src/main/java/com/example/mylang/MainActivity.java
@@ -2,13 +2,25 @@ package com.example.mylang;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class MainActivity extends AppCompatActivity {
+    Handler h = new Handler();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        h.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent goMove = new Intent(MainActivity.this, Home.class);
+                startActivity(goMove);
+                finish();
+            }
+        }, 1000);
     }
 }
