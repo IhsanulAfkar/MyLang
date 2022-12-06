0cafb2b907ca98c35cb6dd5dc9ee37653d124d69
FarisAnwari <mfarisanwari@gmail.com>
2022-12-06 15:38:47 +0700

    intent home ke learn

--- app/src/main/java/com/example/mylang/Home.java
+++ app/src/main/java/com/example/mylang/Home.java
@@ -1,8 +1,11 @@
 package com.example.mylang;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Home extends AppCompatActivity {
 
@@ -10,5 +13,23 @@ public class Home extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        CardView card2 = (CardView) findViewById(R.id.card2);
+
+        card2.setOnClickListener(operasi);
+    }
+
+    View.OnClickListener operasi = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            switch (view.getId()){
+                case R.id.card2:goLvEn();break;
+            }
+        }
+    };
+
+    void goLvEn(){
+        Intent move = new Intent(getBaseContext(), LearnEn.class);
+        startActivityForResult(move, 0);
     }
 }
--- app/src/main/java/com/example/mylang/LearnEn.java
+++ app/src/main/java/com/example/mylang/LearnEn.java
@@ -3,12 +3,50 @@ package com.example.mylang;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 
 public class LearnEn extends AppCompatActivity {
 
+    private ListView listviewEn;
+    private String [] names = {"Alif", "Budi", "Rusa"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_learn_en);
+        listviewEn = findViewById(R.id.listviewEn);
+
+        LvAdapter adapter = new LvAdapter();
+        listviewEn.setAdapter(adapter);
+    }
+
+    public class LvAdapter extends BaseAdapter{
+
+        @Override
+        public int getCount(){
+            return names.length;
+        }
+
+        @Override
+        public Object getItem(int i) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int i) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int i, View view, ViewGroup viewGroup) {
+            view = getLayoutInflater().inflate(R.layout.cardlevel, viewGroup, false);
+            TextView lvName = view.findViewById(R.id.lvName);
+            lvName.setText(names[i]);
+
+            return view;
+        }
     }
 }
